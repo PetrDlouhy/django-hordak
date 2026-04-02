7d3c5fd9db879f3d9022c2f48ddb8dc3c624a30f
Petr Dlouhý <petr.dlouhy@email.cz>
2026-04-02 11:27:37 +0200

    Remove BigAutoField migration churn from checkpoint branch

--- hordak/apps.py
+++ hordak/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 
 class HordakConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = "django.db.models.AutoField"
     name = "hordak"
 
     def ready(self):
